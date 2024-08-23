a5594f0bdb8978fd1097310ec6d5455f69b376a0
MatthieuBlons <matthieu.blons@hotmail.fr>
2024-08-23 15:59:28 +0200

    refactor: formatting

--- scripts/main_tiling.py
+++ scripts/main_tiling.py
@@ -6,14 +6,15 @@ import numpy as np
 from glob import glob
 
 args = get_arguments()
-outpath = os.path.join(args.path_outputs, args.tiler, f'level_{args.level}')
+
+outpath = os.path.join(args.path_outputs, args.tiler, f"level_{args.level}")
 if os.path.isfile(args.path_wsi):
     it = ImageTiler(args=args)
     it.tile_image()
 else:
     dirs = []
-    extensions = set(['.ndpi', '.svs', '.tif'])
-    all_files = glob(os.path.join(args.path_wsi, '*.*'))
+    extensions = set([".ndpi", ".svs", ".tif"])
+    all_files = glob(os.path.join(args.path_wsi, "*.*"))
     files = [f for f in all_files if os.path.splitext(f)[1] in extensions]
     assert len(files) > 0, f"No files with extension {extensions} in {args.path_wsi}"
     for f in files:
@@ -21,15 +22,15 @@ else:
         it = ImageTiler(args=args)
         it.tile_image()
 
-#PCA
-if args.tiler != 'simple':
+# PCA
+if args.tiler != "simple":
     os.chdir(outpath)
-    os.makedirs('pca', exist_ok=True)
-    raw_args = ['--path', '.']
+    os.makedirs("pca", exist_ok=True)
+    raw_args = ["--path", "."]
     ipca = incremental_pca(raw_args)
-    os.makedirs(os.path.join('./mat_pca'), exist_ok=True)
-    files = glob('./mat/*.npy')
+    os.makedirs(os.path.join("./mat_pca"), exist_ok=True)
+    files = glob("./mat/*.npy")
     for f in files:
         m = np.load(f)
         mp = ipca.transform(m)
-        np.save(os.path.join('mat_pca', os.path.basename(f)), mp)
+        np.save(os.path.join("mat_pca", os.path.basename(f)), mp)
