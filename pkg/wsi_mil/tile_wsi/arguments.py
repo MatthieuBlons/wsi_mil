dc819459ded0e3d8f372e5fdf0910f7c6570ecd6
MatthieuBlons <matthieu.blons@hotmail.fr>
2024-08-23 15:54:45 +0200

    feat: add a --config argument to pass .yaml

--- pkg/wsi_mil/tile_wsi/arguments.py
+++ pkg/wsi_mil/tile_wsi/arguments.py
@@ -1,21 +1,82 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import torch
+import yaml
+
 
 def get_arguments():
-    parser = ArgumentParser()
-    parser.add_argument('--path_wsi', required=True, type=str, help="path of the files to downsample (tiff or svs files)")
-    parser.add_argument('--path_mask', type=str, default='no', help='either a path to the xml file, if no, then the whole image is tiled')
-    parser.add_argument('--level', type=int, default = 1, help="scale to which downsample. I.e a scale of 2 means dimensions divided by 2^2")
-    parser.add_argument('--mask_level', type=int, default=-1, help="scale at which has been created the mask. negatives indicate counting levels from the end (slide.level_count)")
-    parser.add_argument('--size', type=int, default = 256, help="size of patches")
-    parser.add_argument('--auto_mask', type=int, default=1, help="if 1, mask is .npy, .xml else")
-    parser.add_argument('--tiler', type=str, default='simple', help='type of tiler : imagenet | simple | simclr | moco')
-    parser.add_argument('--path_outputs', type=str, help='output folder path', default='.')
-    parser.add_argument('--model_path', type=str, default='.', help='if using moco, path to the trained resnet')
-    parser.add_argument('--mask_tolerance', type=float, default=0.75)
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--nf', action='store_true', help='Use this flag when using the nextflow pipeline. Either, dont.')
-    parser.add_argument('--max_nb_tiles', type=int, help='maximum number of tiles to select uniformly. If None, takes all the tiles.', default=None)
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to the config file. If None, use the command line parser",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--path_wsi",
+        type=str,
+        default=".",
+        help="path of the files to downsample (tiff or svs files)",
+    )
+    parser.add_argument(
+        "--path_mask",
+        type=str,
+        default="no",
+        help="either a path to the xml file, if no, then the whole image is tiled",
+    )
+    parser.add_argument(
+        "--level",
+        type=int,
+        default=1,
+        help="scale to which downsample. I.e a scale of 2 means dimensions divided by 2^2",
+    )
+    parser.add_argument(
+        "--mask_level",
+        type=int,
+        default=-1,
+        help="scale at which has been created the mask. negatives indicate counting levels from the end (slide.level_count)",
+    )
+    parser.add_argument("--size", type=int, default=256, help="size of patches")
+    parser.add_argument(
+        "--auto_mask", type=int, default=1, help="if 1, mask is .npy, .xml else"
+    )
+    parser.add_argument(
+        "--tiler",
+        type=str,
+        default="simple",
+        help="type of tiler : imagenet | simple | simclr | moco",
+    )
+    parser.add_argument(
+        "--path_outputs", type=str, help="output folder path", default="."
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default=".",
+        help="if using moco, path to the trained resnet",
+    )
+    parser.add_argument("--mask_tolerance", type=float, default=0.75)
+
+    parser.add_argument(
+        "--nf",
+        action="store_true",
+        help="Use this flag when using the nextflow pipeline. Either, dont.",
+    )
+    parser.add_argument(
+        "--max_nb_tiles",
+        type=int,
+        help="maximum number of tiles to select uniformly. If None, takes all the tiles.",
+        default=None,
+    )
     args = parser.parse_args()
-    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # If there is a config file, we populate args with it (still keeping the default arguments)
+    if args.config is not None:
+        with open(args.config, "r") as f:
+            dic = yaml.safe_load(f)
+        args.__dict__.update(dic)
+    # Set device
+    # args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    args.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
     return args
